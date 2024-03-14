4812b4407b63c8af2bba19c6296a52d021d23e5a
mxrtydluffy <marty.marinda@students.dominican.edu>
2024-03-13 22:20:05 -0700

    [UPDATE] JavaScript Functionality

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,19 @@
 const startBtn = document.querySelector('.start-btn');
 const popupInfo = document.querySelector('.popup-info');
 const exitBtn = document.querySelector('.exit-btn');
+const main = document.querySelector('.main');
+const continueBtn = document.querySelector('.continue-btn');
+const quizSelection = document.querySelector('.quiz-section');
 
 startBtn.onclick = () => {
-    // Once clicked 'active' will display on class name .start-btn
-    popupInfo.classList.add('active')
+    // Once clicked 'active' will display on class name .exit-btn
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
 }
 
-exitBtn.onclick = () => {
-    // Once clicked 'active' will display on class name .start-btn
-    popupInfo.classList.remove('active')
+continueBtn.onclick = () => {
+    // Once clicked 'active' will display on class name .continue-btn
+    quizSelection.classList.add('active');
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
 }
