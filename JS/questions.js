156305c99ec728428aba309975658cb41f58caf5
mxrtydluffy <marty.marinda@students.dominican.edu>
2024-03-13 22:37:58 -0700

    [UPDATE] Display questions on quiz section

---
+++ JS/questions.js
@@ -0,0 +1,88 @@
+let questions = [
+    {
+        numb: 1,
+        question: "How many bones are in the body?",
+        answer: "C. 206",
+        options: [
+            "A. 210",
+            "B. 215",
+            "C. 206",
+            "D. 200"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is a system in the human body?",
+        answer: " A. Digestive",
+        options: [
+            "A. Digestive",
+            "B. Nehronology",
+            "C. Cirumference",
+            "D. Visionology"
+        ]
+    },
+    {
+        numb: 3,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 4,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 5,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 6,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 7,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 8,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 9,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 10,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+] 
--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,7 @@ const exitBtn = document.querySelector('.exit-btn');
 const main = document.querySelector('.main');
 const continueBtn = document.querySelector('.continue-btn');
 const quizSelection = document.querySelector('.quiz-section');
+const quizBox = document.querySelector('.quiz-box');
 
 startBtn.onclick = () => {
     // Once clicked 'active' will display on class name .exit-btn
@@ -16,4 +17,23 @@ continueBtn.onclick = () => {
     quizSelection.classList.add('active');
     popupInfo.classList.remove('active');
     main.classList.remove('active');
+    quizBox.classList.add('active');
+
+    showQuestions(0);
+}
+
+
+let questionCount = 0;
+
+const nextBtn = document.querySelector('.next-btn');
+
+nextBtn.onclick = () => {
+    questionCount++;
+    showQuestions(questionCount);
+}
+
+// Getting Questions and options from the array
+function showQuestions(index)  {
+    const questionText = document.querySelector('.question-text');
+    questionText.textContent = `${questions[index].numb}. ${questions[index].question}`
 }
