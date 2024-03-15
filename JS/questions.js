c94e2c853d10ac8632af05b96c19f2af77eb8b24
mxrtydluffy <marty.marinda@students.dominican.edu>
2024-03-14 23:40:34 -0700

    [ADD] Questions

--- JS/questions.js
+++ JS/questions.js
@@ -12,80 +12,101 @@ let questions = [
     },
     {
         numb: 2,
-        question: "What is a system in the human body?",
-        answer: "A. Digestive",
+        question: "Which of the following is NOT a function of the digestive tract?",
+        answer: "B. To perform gas exchange",
         options: [
-            "A. Digestive",
-            "B. Nehronology",
-            "C. Cirumference",
-            "D. Visionology"
+            "A. To enzymatically break down food",
+            "B. To perform gas exchange",
+            "C. To absorb nutrients into the blood",
+            "D. To eliminate unabsorbed waste"
         ]
     },
     {
         numb: 3,
-        question: "Lorem ipsum",
-        answer: "si",
+        question: "Which anatomical directional term refers to a body part being located toward the front of the body?",
+        answer: "C. Anterior",
         options: [
-            "A. hi",
-            "A. si",
-            "A. lol",
-            "A. pizza"
+            "A. Lateral",
+            "B. Superior",
+            "C. Anterior",
+            "D. Posterior"
         ]
     },
     {
         numb: 4,
-        question: "",
-        answer: "",
+        question: "Where is Vitamin K created in the body?",
+        answer: "C. Large intestine",
         options: [
-
+            "A. Liver",
+            "B. Lungs",
+            "C. Large intestine",
+            "D. Gall bladder"
         ]
     },
     {
         numb: 5,
-        question: "",
-        answer: "",
+        question: "What is erythropoiesis?",
+        answer: "A. Red Blood cell formation",
         options: [
-
+            "A. Red Blood cell formation",
+            "B. White blood cell formation",
+            "C. The destruction of red blood cells",
+            "D. The destruction of white blood cells"
         ]
     },
     {
         numb: 6,
-        question: "",
-        answer: "",
+        question: "A person who has torn their vestus lateralis muscle will need which of the following?",
+        answer: "B. A cane or crutches",
         options: [
-
+            "A. An arm sling",
+            "B. A cane or crutches",
+            "C. A rib support brace",
+            "D. A neck brace"
         ]
     },
     {
         numb: 7,
-        question: "",
-        answer: "",
+        question: "Which is a steroid hormone?",
+        answer: "D. Estrogen",
         options: [
-
+            "A. Insulin",
+            "B. Calcitonin",
+            "C. Adrenaline",
+            "D. Estrogen"
         ]
     },
     {
         numb: 8,
-        question: "",
-        answer: "",
+        question: "Which pair of hormones regulates blood calcium homeostasis?",
+        answer: "B. Parathyroid hormone and calcitonin",
         options: [
-
+            "A. Thyroid hormone and calcitonin",
+            "B. Parathyroid hormone and calcitonin",
+            "C. Insulin and glucagon",
+            "D. Adrenaline and noradrenaline"
         ]
     },
     {
         numb: 9,
-        question: "",
-        answer: "",
+        question: "What type of hormones are follicle-stimulating hormone, thyroid-stimulating hormone, and luteinizing hormone?",
+        answer: "A. Tropic",
         options: [
-
+            "A. Tropic",
+            "B. Secondary",
+            "C. Steroid",
+            "D. Adrenal"
         ]
     },
     {
         numb: 10,
-        question: "",
-        answer: "",
+        question: "Which statement is correct?",
+        answer: "B. The ventral cavity includes the thoracic and abdominopelvic cavities",
         options: [
-
+            "A. The dorsal cavity includes the thoracic and spinal cavities",
+            "B. The ventral cavity includes the thoracic and abdominopelvic cavities",
+            "C. The dorsal cavity includes the spinal and abdominopelvic cavities",
+            "D. The ventral cavity includes the cranial and spinal cavities"
         ]
     },
 ] 
