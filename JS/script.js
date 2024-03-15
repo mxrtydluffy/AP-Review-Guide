a75d1ef152451cfb09e344d91268f10147eaad8b
mxrtydluffy <marty.marinda@students.dominican.edu>
2024-03-15 00:41:47 -0700

    [Troubleshooting] Percentage

--- JS/script.js
+++ JS/script.js
@@ -112,5 +112,25 @@ function headerScore() {
 
 function showResultBox() {
     quizBox.classList.remove('active');
-    quizBox.classList.add('active');
+    resultBox.classList.add('active');
+
+    const scoreText = document.querySelector('.score-text');
+    scoreText.textContent = `Your Score ${userScore} out of ${questions.length}`;
+
+    const circularProgress = document.querySelector('.circular-progress');
+    const progressValue = document.querySelector('.progress-value');
+    let progressStartValue = -1;
+    let progressEndValue = (userScore / questions.length) * 100;
+    let speed = 20;
+    
+    let progress = setInterval(() => {
+        progressStartValue++;
+
+        progressValue.textContent = `${progressStartValue}%`;
+        circularProgress.style.background = `conic-gradient(turquoise ${progressStartValue * 3.6}deg, rgba(255, 255, 255, .1) 0deg)`;
+
+        if (progressStartValue == progressEndValue) {
+            clearInterval(progress);
+        }
+    }, speed);
 }
